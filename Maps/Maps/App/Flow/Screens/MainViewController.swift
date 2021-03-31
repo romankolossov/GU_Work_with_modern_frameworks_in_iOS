d4b53f1ab76acd0a7322618e06f870973c5db561
romankolossov <rkolossov@gmail.com>
2021-03-31 15:06:44 +0300

    hw1 started

---
+++ Maps/Maps/App/Flow/Screens/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  Maps
+//
+//  Created by Roman Kolosov on 31.03.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
