0c34deb8882315d04b769059d0ec8f4e0a9376b0
romankolossov <rkolossov@gmail.com>
2021-03-31 20:16:27 +0300

    hw1 continue

--- Maps/Maps/App/Flow/Screens/MapViewController.swift
+++ Maps/Maps/App/Flow/Screens/MapViewController.swift
@@ -18,17 +18,39 @@ class MapViewController: UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
+    // Центр Москвы
+    private let coordinate = CLLocationCoordinate2D(latitude: 55.753215, longitude: 37.622504)
 
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNavigationVC()
         configureMapVC()
+
+        configureMap()
+        configureMapStyle()
+    }
+
+    // MARK: - Actions
+
+    @objc private func goToRedSquare() {
+        mapView.animate(toLocation: coordinate)
     }
 
     // MARK: - Private methods
 
     // MARK: Configure
+
+    private func configureNavigationVC() {
+        let goToRedSquareItem = UIBarButtonItem(
+            image: UIImage(systemName: "figure.walk"),
+            style: .done,
+            target: self,
+            action: #selector(goToRedSquare)
+        )
+        navigationItem.rightBarButtonItem = goToRedSquareItem
+    }
 
     private func configureMapVC() {
         addSubviews()
@@ -50,5 +72,211 @@ class MapViewController: UIViewController {
         ]
         NSLayoutConstraint.activate(mapViewConstraints)
     }
+
+    private func configureMap() {
+        // Создаём камеру с использованием координат и уровнем увеличения
+        let camera = GMSCameraPosition.camera(withTarget: coordinate, zoom: 17)
+        // Устанавливаем камеру для карты
+        mapView.camera = camera
+    }
+
+    private func configureMapStyle() {
+           let style = "[" +
+               "  {" +
+               "    \"featureType\": \"all\"," +
+               "    \"elementType\": \"geometry\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#242f3e\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"all\"," +
+               "    \"elementType\": \"labels.text.stroke\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"lightness\": -80" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"administrative\"," +
+               "    \"elementType\": \"labels.text.fill\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#746855\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"administrative.locality\"," +
+               "    \"elementType\": \"labels.text.fill\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#d59563\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"poi\"," +
+               "    \"elementType\": \"labels.text.fill\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#d59563\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"poi.park\"," +
+               "    \"elementType\": \"geometry\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#263c3f\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"poi.park\"," +
+               "    \"elementType\": \"labels.text.fill\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#6b9a76\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"road\"," +
+               "    \"elementType\": \"geometry.fill\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#2b3544\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"road\"," +
+               "    \"elementType\": \"labels.text.fill\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#9ca5b3\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"road.arterial\"," +
+               "    \"elementType\": \"geometry.fill\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#38414e\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"road.arterial\"," +
+               "    \"elementType\": \"geometry.stroke\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#212a37\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"road.highway\"," +
+               "    \"elementType\": \"geometry.fill\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#746855\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"road.highway\"," +
+               "    \"elementType\": \"geometry.stroke\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#1f2835\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"road.highway\"," +
+               "    \"elementType\": \"labels.text.fill\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#f3d19c\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"road.local\"," +
+               "    \"elementType\": \"geometry.fill\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#38414e\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"road.local\"," +
+               "    \"elementType\": \"geometry.stroke\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#212a37\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"transit\"," +
+               "    \"elementType\": \"geometry\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#2f3948\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"transit.station\"," +
+               "    \"elementType\": \"labels.text.fill\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#d59563\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"water\"," +
+               "    \"elementType\": \"geometry\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#17263c\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"water\"," +
+               "    \"elementType\": \"labels.text.fill\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"color\": \"#515c6d\"" +
+               "      }" +
+               "    ]" +
+               "  }," +
+               "  {" +
+               "    \"featureType\": \"water\"," +
+               "    \"elementType\": \"labels.text.stroke\"," +
+               "    \"stylers\": [" +
+               "      {" +
+               "        \"lightness\": -20" +
+               "      }" +
+               "    ]" +
+               "  }" +
+           "]"
+           do {
+               mapView.mapStyle = try GMSMapStyle(jsonString: style)
+           } catch {
+               print(error)
+           }
+       }
 
 }
